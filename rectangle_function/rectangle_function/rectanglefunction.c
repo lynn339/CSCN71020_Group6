1a9f86e36f13f1adc08e6b724857815dd5037a0b
lynn339 <lynnchien0419@gmail.com>
2025-11-16 16:06:20 -0500

    add int main() to run the function

--- rectangle_function/rectangle_function/rectanglefunction.c
+++ rectangle_function/rectangle_function/rectanglefunction.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-void getPoints(int* x, int* y)
+void getPoints(int *x, int *y)
 {
-    printf("Enter a point (x, y): ");
-    while (scanf("%d , %d", x, y) != 2)
+    while (scanf_s("%d , %d", x, y) != 2)
     {
         printf("Invalid value, please enter a point (x, y): ");
         while (getchar() != '\n');
     }
+}
+
+int main()
+{
+    int x[4], y[4];
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        printf("Enter point %d (x, y) : ", i + 1);
+        getPoints(&x[i], &y[i]);
+        
+    }
+    for (i = 0; i < 4; i++)
+    {
+        printf("The point %d is %d %d\n", i + 1, x[i], y[i]);
+    }
+    return 0;
+    
 }
