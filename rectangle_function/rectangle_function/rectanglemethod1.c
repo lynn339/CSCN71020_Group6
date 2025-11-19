0108839fa07dbe9e9629fa75fee7444ac7550cec
Drayvenice <s1124110064@gmail.com>
2025-11-18 23:17:51 -0500

    Add Point struct and distance function

--- rectangle_function/rectangle_function/rectanglemethod1.c
+++ rectangle_function/rectangle_function/rectanglemethod1.c
@@ -1,1 +1,13 @@
-#
+#include <stdio.h>
+#include <math.h>
+
+
+typedef struct {
+    double x;
+    double y;
+} Point;
+
+// calculate the distance between two points
+double distance(Point a, Point b) {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+}
